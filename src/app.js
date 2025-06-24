003087b8d55583aba235d2bef933b31296c4fa5c
sudhanshu2070 <sssallen2016@gmail.com>
2025-06-24 23:21:56 +0530

    the data handler API 0.2

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/v1', routes); // Base route prefix
+
+module.exports = app;
