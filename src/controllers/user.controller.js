29719d6b07815ebe0f83a01f9cbec1ebaeb5241c
sudhanshu2070 <sssallen2016@gmail.com>
2025-06-25 01:14:45 +0530

    the data handler API 0.5

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,25 @@
+const users = [
+    { id: 1, name: 'Naruto Uzumaki', salary: 50000 },
+    { id: 2, name: 'Sasuke Uchiha', salary: 52000 },
+    { id: 3, name: 'Sakura Haruno', salary: 48000 },
+    { id: 4, name: 'Kakashi Hatake', salary: 70000 },
+    { id: 5, name: 'Shikamaru Nara', salary: 55000 },
+    { id: 6, name: 'Hinata Hyuga', salary: 47000 },
+    { id: 7, name: 'Rock Lee', salary: 46000 },
+    { id: 8, name: 'Neji Hyuga', salary: 49000 },
+    { id: 9, name: 'Tenten', salary: 45000 },
+    { id: 10, name: 'Ino Yamanaka', salary: 47000 },
+    { id: 11, name: 'Choji Akimichi', salary: 46000 },
+    { id: 12, name: 'Kiba Inuzuka', salary: 45500 },
+    { id: 13, name: 'Shino Aburame', salary: 46500 },
+    { id: 14, name: 'Gaara', salary: 68000 },
+    { id: 15, name: 'Temari', salary: 50000 },
+    { id: 16, name: 'Kankuro', salary: 49500 },
+    { id: 17, name: 'Tsunade', salary: 90000 }
+];
+
 exports.getAllUsers = (req, res) => {
-  res.json({ message: 'Get all users' });
+  res.json({ users });
 };
 
 exports.createUser = (req, res) => {
