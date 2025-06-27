d10b0a88c81bac83ab42cfac57f664eabdef98cb
sudhanshu2070 <sssallen2016@gmail.com>
2025-06-27 23:26:05 +0530

    Rate Limiter 0.2

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
 const { timeLogger, errorLogger } = require('./middlewares/logger');
+const rateLimiter = require('./middlewares/rateLimiter'); 
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use(rateLimiter); // Middleware to limit request rate
 app.use(timeLogger); // Middleware to log request time
 app.use(errorLogger); // Middleware to log errors
 
