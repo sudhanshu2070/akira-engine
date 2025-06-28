1d8986bda6c582a7f8b9276b8c900632d0a61599
sudhanshu2070 <sssallen2016@gmail.com>
2025-06-28 21:04:57 +0530

    File upload via Multer 0.2

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const userRoutes = require('./user.routes');
+const fileUploadRoutes = require('./uploadFile.routes');
 
 router.use('/users', userRoutes);
+router.use('/upload', require('./uploadFile.routes'));
 
 module.exports = router;
