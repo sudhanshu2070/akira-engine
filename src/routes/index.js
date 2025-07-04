3f50101064b0d65304805ba1575dfb2b367dce5f
sudhanshu2070 <sssallen2016@gmail.com>
2025-07-04 21:39:53 +0530

    Map, filters and all with the sample data 0.4

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const userRoutes = require('./user.routes');
 const fileUploadRoutes = require('./uploadFile.routes');
+const testRoutes = require('./test.route'); 
 
 router.use('/users', userRoutes);
 router.use('/upload', fileUploadRoutes);
+router.use('/testWithSampleData', testRoutes);
 
 module.exports = router;
