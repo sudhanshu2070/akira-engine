88bb87c267335594ef347b76e010872f75ba093e
sudhanshu2070 <sssallen2016@gmail.com>
2025-07-07 23:07:55 +0530

    Push notification 0.3

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const userRoutes = require('./user.routes');
 const fileUploadRoutes = require('./uploadFile.routes');
 const testRoutes = require('./test.route'); 
+const pushRoutes = require('./push.route');
 
 router.use('/users', userRoutes);
 router.use('/upload', fileUploadRoutes);
 router.use('/testWithSampleData', testRoutes);
+router.use('/push', pushRoutes);
 
 module.exports = router;
